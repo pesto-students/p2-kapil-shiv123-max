3f55451f5ae62f7079ff9712f159b140fbaeedc5
shiv123-max <shivanshusid123@gmail.com>
2022-09-04 14:30:33 +0530

    some error checks added

--- Week-15/LLD/api/cli.js
+++ Week-15/LLD/api/cli.js
@@ -2,7 +2,6 @@ import promptSync from "prompt-sync";
 import parkingLot from "../models/parkingLot.js";
 import car from "../models/car.js";
 
-
 const prompt = promptSync({ sigint: true });
 let parkingLotCreated = false;
 let parkingLotInstance = null;
@@ -13,21 +12,39 @@ while (!exit) {
   switch (command.split(" ")[0]) {
     case "create_parking_lot":
       {
+        if (args.length < 1) {
+          console.log(
+            `Invalid number of arguments. Required: 1 Given: ${args.length}`
+          );
+          break;
+        }
         parkingLotInstance = new parkingLot(Number(args[0]));
         parkingLotCreated = true;
       }
       break;
     case "park":
       {
+        if (args.length < 2) {
+          console.log(
+            `Invalid number of arguments. Required: 2 Given: ${args.length}`
+          );
+          break;
+        }
         if (!parkingLotCreated) {
           console.log("No parking lot has been created.");
           break;
         }
         parkingLotInstance.park(new car(args[0], args[1]));
       }
-      break; 
+      break;
     case "leave":
       {
+        if (args.length < 1) {
+          console.log(
+            `Invalid number of arguments. Required: 1 Given: ${args.length}`
+          );
+          break;
+        }
         if (!parkingLotCreated) {
           console.log("No parking lot has been created.");
           break;
@@ -44,6 +61,12 @@ while (!exit) {
     }
     case "registration_numbers_for_cars_with_colour":
       {
+        if (args.length < 1) {
+          console.log(
+            `Invalid number of arguments. Required: 1 Given: ${args.length}`
+          );
+          break;
+        }
         if (!parkingLotCreated) {
           console.log("No parking lot has been created.");
           break;
@@ -53,6 +76,12 @@ while (!exit) {
       break;
     case "slot_numbers_for_cars_with_colour":
       {
+        if (args.length < 1) {
+          console.log(
+            `Invalid number of arguments. Required: 1 Given: ${args.length}`
+          );
+          break;
+        }
         if (!parkingLotCreated) {
           console.log("No parking lot has been created.");
           break;
@@ -62,20 +91,25 @@ while (!exit) {
       break;
     case "slot_number_for_registration_number":
       {
+        if (args.length < 1) {
+          console.log(
+            `Invalid number of arguments. Required: 1 Given: ${args.length}`
+          );
+          break;
+        }
         if (!parkingLotCreated) {
           console.log("No parking lot has been created.");
           break;
         }
         parkingLotInstance.filterSlots("registration number", args[0]);
-      } 
+      }
       break;
     case "exit": {
-        exit = true;
-        break;
+      exit = true;
+      break;
     }
     default: {
       console.log(`Invalid command ${command}`);
     }
   }
 }
-
